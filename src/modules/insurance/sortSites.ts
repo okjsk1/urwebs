e5e31d1be11eb5497acc6012797da5dee5ec7750
okjsk1 <okjsk1@gmail.com>
2025-09-13 16:13:39 +0900

    chore: remove legacy insurance persona pages

--- src/modules/insurance/sortSites.ts
+++
@@ -1,3 +0,0 @@
-import type { Site } from "./taxonomy";
-const order = { public:1, association:2, insurer:3, compare:4, edu:5, stats:6, tool:7, community:8 } as const;
-export const sortSites = (a:Site,b:Site) => (order[a.sourceType]??99)-(order[b.sourceType]??99);
